90893e29adfff3dd847d1e82a27c6858d23c908f
Naoyuki Umeda <naoyuki_3075@yahoo.co.jp>
2025-11-01 01:59:55 +0900

    User.go DB作成

--- go.mod
+++ go.mod
@@ -2,6 +2,9 @@ module go-todoapp
 
 go 1.25.3
 
-require gopkg.in/go-ini/ini.v1 v1.67.0
+require (
+	github.com/mattn/go-sqlite3 v1.14.32
+	gopkg.in/go-ini/ini.v1 v1.67.0
+)
 
 require github.com/stretchr/testify v1.11.1 // indirect
--- main.go
+++ main.go
@@ -1,16 +1,15 @@
 package main
 
 import (
-	"fmt"
-	"go-todoapp/config"
+	"go-todoapp/app/models"
 	"log"
 )
 
 func main() {
-	fmt.Println(config.Config.Port)
-	fmt.Println(config.Config.SQLDriver)
-	fmt.Println(config.Config.DbName)
-	fmt.Println(config.Config.LogFile)
+	// fmt.Println(config.Config.Port)
+	// fmt.Println(config.Config.SQLDriver)
+	// fmt.Println(config.Config.DbName)
+	// fmt.Println(config.Config.LogFile)
 
-	log.Println("test")
+	log.Println(models.Db)
 }
